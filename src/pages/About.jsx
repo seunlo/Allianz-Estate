13a92dfafe0514a1d44bb130ac6396665b386471
seun jacobs <seunlo@gmail.com>
2024-02-15 11:16:38 +0100

    allianz estate project completed

---
+++ src/pages/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Allianz Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam autem
+        laboriosam explicabo est cum inventore id suscipit, eveniet nam
+        pariatur. Quod, maxime? Cupiditate ipsum repellendus soluta velit
+        mollitia fuga quaerat. Voluptas doloribus minima molestiae dolor
+        recusandae ullam odit ex perspiciatis optio quaerat veritatis
+        reprehenderit, ipsum porro adipisci repudiandae aliquid corporis minus
+        vitae ea hic natus possimus non. Possimus, earum perspiciatis.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam autem
+        laboriosam explicabo est cum inventore id suscipit, eveniet nam
+        pariatur. Quod, maxime? Cupiditate ipsum repellendus soluta velit
+        mollitia fuga quaerat. Voluptas doloribus minima molestiae dolor
+        recusandae ullam odit ex perspiciatis optio quaerat veritatis
+        reprehenderit, ipsum porro adipisci repudiandae aliquid corporis minus
+        vitae ea hic natus possimus non. Possimus, earum perspiciatis.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam autem
+        laboriosam explicabo est cum inventore id suscipit, eveniet nam
+        pariatur. Quod, maxime? Cupiditate ipsum repellendus soluta velit
+        mollitia fuga quaerat. Voluptas doloribus minima molestiae dolor
+        recusandae ullam odit ex perspiciatis optio quaerat veritatis
+        reprehenderit, ipsum porro adipisci repudiandae aliquid corporis minus
+        vitae ea hic natus possimus non. Possimus, earum perspiciatis.
+      </p>
+    </div>
+  );
+}
